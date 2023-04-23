9858a3e3fb332f4727ffb9359fe52ed249dc36b8
kenneth o cruz <cruzkenneth504@gmail.com>
2023-04-23 15:50:10 -0700

    Shapes have been render

--- main/lib/shapes.js
+++ main/lib/shapes.js
@@ -6,7 +6,7 @@ class Shape {
     
   }
 
-  addColor(color){
+  shapeColor(color){
    this.color = color 
   }
 
@@ -23,23 +23,24 @@ class Shape {
 
 class Circle extends Shape {
   render(){
-    return 
+    return `<svg width="300px" height="200px"><circle cx="150" cy="100" r="80" fill="${this.color}" stroke="black" stroke-width="3" /><text x="150" y="100" font-size="50" text-anchor="middle" fill="${this.textColor}" alignment-baseline="central">${this.text}</text></svg>`
   }
 }
 
 class Triangle extends Shape {
   render(){
-   return 
+    return `<svg width="300" height="200"><polygon points="150 0, 300 200, 0 200" style="fill:${this.color};"/><text x="50%" y="80%" dominant-baseline="middle" font-size="70px" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
+  
           
   }
 }
 
 class Square extends Shape {
   render(){
-   return  
+   return `<svg width="300" height="200"><rect width="300" height="200" style="fill:${this.color};stroke-width:2;stroke:#FFFFFF"/><text x="50%" y="50%" dominant-baseline="middle" font-size="100px" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
 
   }
 }
 
-console.log(Shape)
+
 modules.exports = { Circle, Triangle, Square };
