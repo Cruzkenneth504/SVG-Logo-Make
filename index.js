fc4cef5219268de68e29ddf48ee21325b7c58f86
kenneth o cruz <cruzkenneth504@gmail.com>
2023-04-23 19:34:30 -0700

    Logo generates

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
-const  { Circle, Triangle, Square } = require('./shapes')
-const inquirer = require('inquire');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const { Circle, Triangle, Square } = require('./shapes');
 
-// inquirer.prompt([
-//   {
-//     type: 'input',
-//     name: 'text',
-//     message: 'Enter up to three characters for the logo text:'
-//   },
-//   {
-//     type: 'input',
-//     name: 'textColor',
-//     message: 'Enter a color keyword or hexadecimal number for the text color:'
-//   },
-//   {
-//     type: 'list',
-//     name: 'shape',
-//     message: 'Choose a shape for the logo:',
-//     choices: ['circle', 'triangle', 'square']
-//   },
-//   {
-//     type: 'input',
-//     name: 'shapeColor',
-//     message: 'Enter a color keyword or hexadecimal number for the shape color:'
-//   }
-// ]).then((answers) => {
-//   let shape;
+inquirer.prompt([
+  {
+    name: 'text',
+    message: 'Enter up to three characters:'
+  },
+  {
+    name: 'textColor',
+    message: 'Enter the text color (color keyword or hexadecimal number):'
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape:',
+    choices: ['circle', 'triangle', 'square']
+  },
+  {
+    name: 'shapeColor',
+    message: 'Enter the shape color (color keyword or hexadecimal number):'
+  }
+]).then(answers => {
+  let shape;
+  switch (answers.shape) {
+    case 'circle':
+      shape = new Circle();
+      break;
+    case 'triangle':
+      shape = new Triangle();
+      break;
+    case 'square':
+      shape = new Square();
+      break;
+  }
+  shape.shapeColor(answers.shapeColor);
+  shape.addText(answers.text);
+  shape.addTextColor(answers.textColor);
 
-//   switch (answers.shape) {
-//     case 'circle':
-//       shape = new Circle();
-//       break;
-//     case 'triangle':
-//       shape = new Triangle();
-//       break;
-//     case 'square':
-//       shape = new Square();
-//       break;
-//   }
-
-//   shape.shapeColor(answers.shapeColor);
-//   shape.addText(answers.text);
-//   shape.addTextColor(answers.textColor);
-
-//   const svg = shape.render();
-//   fs.writeFileSync('logo.svg', svg);
-//   console.log('Generated logo.svg');
-// }).catch((error) => {
-//   console.error(error);
-// });
+  const svg = shape.render();
+  fs.writeFile('logo.svg', svg, (err) => {
+    if (err) throw err;
+    console.log('Generated logo.svg');
+  });
+});
